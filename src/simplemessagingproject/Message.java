7a8e73b165e696f8d9bb3084209492695e4e095b
Unknown <brennon.gee@gmail.com>
2018-09-26 07:27:34 -0400

    Deleted main class and created message class

--- src/simplemessagingproject/Message.java
+++ src/simplemessagingproject/Message.java
@@ -9,13 +9,6 @@ package simplemessagingproject;
  *
  * @author Brennon
  */
-public class SimpleMessagingProject {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Message {
     
 }
