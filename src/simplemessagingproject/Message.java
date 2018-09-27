9d409372815e2072453573cd63308c43b4101147
Unknown <brennon.gee@gmail.com>
2018-09-26 20:25:48 -0400

    Created classes

--- src/simplemessagingproject/Message.java
+++ src/simplemessagingproject/Message.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package simplemessagingproject;
 
 /**
@@ -11,4 +6,16 @@ package simplemessagingproject;
  */
 public class Message {
     
+
+    public void ErrorMessage() {
+        
+    }
+
+    public void SystemMessage() {
+        
+    }
+
+    public void chatMessage() {
+        
+    }
 }
