1d6778bf0a388130345f1a878a4536e4a24dd79a
Unknown <brennon.gee@gmail.com>
2018-09-26 21:03:44 -0400

    Created methods in the server class

--- src/simplemessagingproject/SMPServer.java
+++ src/simplemessagingproject/SMPServer.java
@@ -1,9 +1,63 @@
 package simplemessagingproject;
 
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 /**
  *
  * @author Brennon
  */
 public class SMPServer {
     
+    private int uniqueID;
+    
+    private int port;
+    
+    private ArrayList<> al;
+    
+    private SimpleDateFormat sdf;
+    
+    private boolean keepRunning;
+    
+    
+    
+    public SMPServer(int port) {
+        
+        this.port = port;
+        
+        sdf = new SimpleDateFormat("HH:mm:ss");
+        
+        al = new ArrayList<>();
+    }
+    
+    public void run (){
+        
+        keepRunning = true;
+        
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            while(keepRunning){
+                Socket socket = serverSocket.accept(); 
+            }
+        }
+        catch(IOException e){
+            display("Exception closing the server and clients: " + e);
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+
+        
+    }
 }
