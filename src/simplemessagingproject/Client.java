be76327302e37f268b345b83567251a92420979f
Unknown <brennon.gee@gmail.com>
2018-09-27 22:07:58 -0400

    updating client class

--- src/simplemessagingproject/Client.java
+++ src/simplemessagingproject/Client.java
@@ -5,8 +5,11 @@ import java.text.SimpleDateFormat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  *
@@ -18,16 +21,26 @@ public class Client {
     
     private String host;
     
+    private String username;
+    
     private SimpleDateFormat sdf;
     
     private boolean keepRunning;
     
-    private void Client (int port, String host){
-        this.port = 1500;
-        this.host = "localhost";
+    private ObjectInputStream sInput;
+    
+    private ObjectOutputStream sOutput;
+    
+    private Socket socket;
+
+    
+    Client (int port, String username, String host){
+        this.username = username;
+        this.port = port;
+        this.host = host;
     }
     
-    public void run(){
+    public boolean run(){
         System.out.println("Creating socket to '" + host + "' on port " + port);
         try {
             
@@ -39,17 +52,64 @@ public class Client {
             } 
         }
         catch (IOException e){
-            
+            close();
+            return false;
         }
+        return true;
+    }
+    
+    public void close(){
+        try {
+            if(sInput != null) sInput.close();
+        }
+        catch(IOException e) {} 
+        try {
+            if(sOutput != null) sOutput.close();
+        }
+        catch(IOException e) {} 
+        try{
+            if(socket != null) socket.close();
+        }
+        catch(IOException e) {} 
+
     }
     
     
     public static void main(String args[]){
-        int port = 2;
-        String host = "localhost";
-               
-        Client client = new Client();
-        client.run();
+        if (args.length > 2 || args.length < 3){
+            System.out.println("Not enough arguments");
+            System.out.println("Usage: Client [port] [server address] [username] ");
+        }
         
+        int cPort = Integer.parseInt(args[1]);
+        String sHost = args[2];
+        String cUsername = args[3];
+        
+        Client client = new Client(cPort, sHost, cUsername);
+        if (!client.run()){
+            System.out.println("Could not connect to the server ");
+            return;
+        }
+        
+        Scanner in = new Scanner(System.in);
+        
+        while (true){
+            System.out.println("> ");
+            String message = in.nextLine();
+            
+            if(message.equalsIgnoreCase("LOGOUT")){
+                break;
+            }
+            else if (message.equalsIgnoreCase("HELP")){
+                
+            }
+            else if (message.equalsIgnoreCase("USERS")){
+                
+            }
+            else{
+                
+            }
+            client.close();
+        }
     }
 }
